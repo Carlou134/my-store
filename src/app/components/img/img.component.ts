043b46590fc2f2b8ab5cf2d5fe7e130857a3aaaa
Carlou134 <carlouvasquez134@gmail.com>
2025-10-24 21:38:28 -0500

    input e imagenes

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-img',
   standalone: true,
-  imports: [],
+  imports: [ NgIf ],
   templateUrl: './img.component.html',
   styleUrl: './img.component.scss'
 })
 export class ImgComponent {
+  @Input() img:string = "valor inicial";
 
 }
