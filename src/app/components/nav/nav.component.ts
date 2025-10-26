59badd9274d3d52d138ea9c06ab75f7c288311a9
Carlou134 <carlouvasquez134@gmail.com>
2025-10-26 13:18:25 -0500

    reactividad

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StoreService } from '../../services/store.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,11 +11,16 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
 
   activeMenu = false;
+  counter = 0;
 
-  constructor() {}
+  constructor(
+    private storeService:StoreService
+  ) {}
 
   ngOnInit(): void {
-
+    this.storeService.myCart$.subscribe(products => {
+      this.counter = products.length;
+    });
   }
 
   toggleMenu(){
